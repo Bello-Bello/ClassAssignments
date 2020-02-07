468eeea76e13f9b91f99969712cd8b87d7a71c08
iBello <iBello@iBelloPC>
2020-02-07 18:21:51 -0500

    Re-edited code

--- AssignmentOne/src/SpltStrings.java
+++ AssignmentOne/src/SpltStrings.java
@@ -1,40 +1,35 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
-public class SpltStrings {
+public class SplitStrings {
+    public static void main(String[] args) throws IOException {
 
-	package com.jetBello;
+        BufferedReader br = new BufferedReader(new FileReader("le.txt"));
+        String str = br.readLine();
+        String specialCharacters=" !#$%&'()*+,-./:;<=>?@[]^_`{|}";
+        while(str != null) {
+            String[] split = str.split(" ");
+            for(int i = 0 ; i < split.length ; i++) {
+                String lex = split[i];
+                int count = 0;
+                while(count < lex.length()) {
+                    if(specialCharacters.contains(Character.toString(lex.charAt(count)))) {
+                        if(!specialCharacters.contains(Character.toString(lex.charAt(count-1)))){
+                            System.out.println();
+                        }
 
-	import java.io.BufferedReader;
-	import java.io.FileNotFoundException;
-	import java.io.FileReader;
-	import java.io.IOException;
-
-	public class SplitStrings {
-	    public static void main(String[] args) throws IOException {
-
-	        BufferedReader br = new BufferedReader(new FileReader("le.txt"));
-	        String str = br.readLine();
-	        String specialCharacters=" !#$%&'()*+,-./:;<=>?@[]^_`{|}";
-	        while(str != null) {
-	            String[] split = str.split(" ");
-	            for(int i = 0 ; i < split.length ; i++) {
-	                String lex = split[i];
-	                int count = 0;
-	                while(count < lex.length()) {
-	                    if(specialCharacters.contains(Character.toString(lex.charAt(count)))) {
-	                        if(!specialCharacters.contains(Character.toString(lex.charAt(count-1)))){
-	                            System.out.println();
-	                        }
-
-	                        System.out.println(lex.charAt(count));
-	                    }
-	                    else{
-	                        System.out.print(lex.charAt(count));
-	                    }
-	                    count++;
-	                }
-	                System.out.println();
-	            }
-	            str = br.readLine();
-	        }
-	    }
-	}
+                        System.out.println(lex.charAt(count));
+                    }
+                    else{
+                        System.out.print(lex.charAt(count));
+                    }
+                    count++;
+                }
+                System.out.println();
+            }
+            str = br.readLine();
+        }
+    }
+}
