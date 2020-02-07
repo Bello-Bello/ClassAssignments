cd4bce5b5912f6d15be65c852dee9e9f2539872e
iBello <iBello@iBelloPC>
2020-02-07 18:19:37 -0500

    SecondEdit

--- AssignmentOne/src/SpltStrings.java
+++ AssignmentOne/src/SpltStrings.java
@@ -1,9 +1,40 @@
 
 public class SpltStrings {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	package com.jetBello;
 
+	import java.io.BufferedReader;
+	import java.io.FileNotFoundException;
+	import java.io.FileReader;
+	import java.io.IOException;
+
+	public class SplitStrings {
+	    public static void main(String[] args) throws IOException {
+
+	        BufferedReader br = new BufferedReader(new FileReader("le.txt"));
+	        String str = br.readLine();
+	        String specialCharacters=" !#$%&'()*+,-./:;<=>?@[]^_`{|}";
+	        while(str != null) {
+	            String[] split = str.split(" ");
+	            for(int i = 0 ; i < split.length ; i++) {
+	                String lex = split[i];
+	                int count = 0;
+	                while(count < lex.length()) {
+	                    if(specialCharacters.contains(Character.toString(lex.charAt(count)))) {
+	                        if(!specialCharacters.contains(Character.toString(lex.charAt(count-1)))){
+	                            System.out.println();
+	                        }
+
+	                        System.out.println(lex.charAt(count));
+	                    }
+	                    else{
+	                        System.out.print(lex.charAt(count));
+	                    }
+	                    count++;
+	                }
+	                System.out.println();
+	            }
+	            str = br.readLine();
+	        }
+	    }
 	}
-
-}
